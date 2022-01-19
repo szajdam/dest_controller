28f4b90430c8e8b5d8ba9883d714b0acdf67e8e9
Marcin Szajda <mszajda@bravurasolutions.com>
2022-01-19 16:48:00 +0100

    Initail

---
+++ test.py
@@ -0,0 +1,12 @@
+import display
+
+display.start()
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
+display.set_values('12.0', '12.11', '12.12')
