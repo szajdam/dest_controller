787d30fad1044f40ae2b1143bf336c0c58a4e90b
Marcin Szajda <mszajda@bravurasolutions.com>
2022-01-20 17:04:51 +0100

    temp reader refactor

---
+++ temperature/temperature_reader.py
@@ -0,0 +1,28 @@
+import machine
+import onewire
+import ds18x20
+
+
+class TempReader:
+    temperature = None
+
+    def __init__(self, pin_number):
+        self.temperature = 0.00
+        pin = machine.Pin(pin_number)
+        self.ds = ds18x20.DS18X20(onewire.OneWire(pin))
+        self.roms = self.ds.scan()
+        print('found devices:', self.roms)
+
+    def get_temperature_for_string(self):
+        if self.temperature is not None:
+            return "{:.2f}".format(self.temperature)
+        else:
+            return "00.00"
+
+    def read_temperature(self):
+        if len(self.roms) > 0:
+            self.ds.convert_temp()
+            for rom in self.roms:
+                self.temperature = self.ds.read_temp(rom);
+        else:
+            self.temperature = None
--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import time
-import display
-import temperature_reader
+from display import display
+from temperature import temperature_reader_factory
 
 display.start()
-temp_reader = temperature_reader.TempReader()
-temperature = temp_reader.read_temperature()
+temp_reader_keg = temperature_reader_factory.get_temp_reader_for_keg()
+temp_reader_col = temperature_reader_factory.get_temp_reader_for_column()
+temp_reader_cool = temperature_reader_factory.get_temp_reader_for_cooler()
+
 while True:
-    temperature = "{:.2f}".format(temp_reader.read_temperature())
-    display.set_values(temperature, '12.11', '12.12')
+    temperature_keg = temp_reader_keg.get_temperature_for_string()
+    temperature_col = temp_reader_col.get_temperature_for_string()
+    temperature_cool = temp_reader_cool.get_temperature_for_string()
+    display.set_values(temperature_keg, temperature_col, temperature_cool)
     time.sleep_ms(1000)
